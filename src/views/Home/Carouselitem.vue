0288f27498a7d844a183d16fe5b4b6a074dbffb0
milkgan <394717060@qq.com>
2022-03-24 00:17:57 +0800

    commit

--- src/views/Home/Carouselitem.vue
+++ src/views/Home/Carouselitem.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="carousel-item-container">
-        <div class="carousel-img">
+    <div class="carousel-item-container" ref="container">
+        <div class="carousel-img" ref="image">
             <ImageLoader 
             @load="this.showWords"
             :src="carousel.bigImg" 
@@ -23,30 +23,61 @@ export default {
     data() {
         return {
             titleWidth: 0,
-            descWidth: 0
+            descWidth: 0,
+            containerSize: null,// 外层容器尺寸
+            innerSize: null,// 里层图片尺寸
+            mouseX: 0,// 鼠标的横坐标
+            mouseY: 0,// 鼠标的纵坐标
+
         }
     },
     mounted() {
         this.titleWidth = this.$refs.title.clientWidth;
         this.descWidth = this.$refs.desc.clientWidth;
+        this.setSize();
+        this.mouseX = this.center.x;
+        this.mouseY = this.center.y;
+    },
+    computed: {
+        // 获取到中心坐标
+        center() {
+            return {
+                x: this.containerSize.width / 2,
+                y: this.containerSize.height / 2
+            }
+        }
     },
     methods: {
         // 显示文字
         showWords() {
             this.$refs.title.style.opacity = 1;
             this.$refs.title.style.width = 0;
+            // 强制让浏览器渲染一次
             this.$refs.title.clientWidth;
             this.$refs.title.style.transition = '1s';
             this.$refs.title.style.width = this.titleWidth + 'px';
 
             this.$refs.desc.style.opacity = 1;
             this.$refs.desc.style.width = 0;
+             // 强制让浏览器渲染一次
             this.$refs.desc.clientWidth;
             this.$refs.desc.style.transition = '2s 1s';
             this.$refs.desc.style.width = this.descWidth + 'px';
-
-
         },
+        // 设置尺寸
+        setSize() {
+            this.containerSize = {
+                width: this.$refs.container.clientWidth,
+                height: this.$refs.container.clientHeight,
+            }
+            console.log('ccc',this.containerSize)
+            this.innerSize = {
+                width: this.$refs.image.clientWidth,
+                height: this.$refs.image.clientHeight,
+            }
+            console.log('iii',this.innerSize)
+            
+        }
     }
 
 }
